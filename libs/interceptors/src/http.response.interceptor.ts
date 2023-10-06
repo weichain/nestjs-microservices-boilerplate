ed2ac4aa7bbde19db8687cab2541fc1be349ce7f
Krasimir Raykov <raykov@weichain.tech>
2023-10-06 15:18:57 +0300

    feat: disable interceptor for metrics

--- libs/interceptors/src/http.response.interceptor.ts
+++ libs/interceptors/src/http.response.interceptor.ts
@@ -1,5 +1,6 @@
 import { DefaultStatusMessage } from '@lib/common';
 import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,7 +12,12 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class HttpResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
     const response = context.switchToHttp().getResponse();
+
+    if (request.url === '/metrics') {
+      return next.handle();
+    }
 
     return next.handle().pipe(
       map((data: any) => {
