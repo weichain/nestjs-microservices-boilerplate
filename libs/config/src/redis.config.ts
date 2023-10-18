506d0d07743e09c0821fa885e481050c3b90e850
Krasimir Raykov <raykov@weichain.tech>
2023-10-18 18:08:57 +0300

    fix: add devDefault for redis password

--- libs/config/src/redis.config.ts
+++ libs/config/src/redis.config.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, port, str } from 'envalid';
 
 export default cleanEnv(process.env, {
-  REDIS_PASSWORD: str({ devDefault: '' }),
+  REDIS_PASSWORD: str({ devDefault: 'root' }),
   REDIS_HOST: str({ devDefault: 'localhost' }),
   REDIS_PORT: port({ devDefault: 6379 }),
   REDIS_TLS: str({ devDefault: '' }),
