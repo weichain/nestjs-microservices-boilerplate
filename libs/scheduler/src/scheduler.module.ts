13c8a3e73863795ff354b6e4f49db01deeb6b5c0
Krasimir Raykov <raykov@weichain.tech>
2023-10-19 14:36:22 +0300

    feat: add scheduler impl

--- libs/scheduler/src/scheduler.module.ts
+++ libs/scheduler/src/scheduler.module.ts
@@ -1,7 +1,11 @@
+import { LoggerModule } from '@lib/logger';
+import { PrismaModule } from '@lib/prisma';
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerService } from './scheduler.service';
 
 @Module({
+  imports: [LoggerModule, PrismaModule, ScheduleModule.forRoot()],
   providers: [SchedulerService],
   exports: [SchedulerService],
 })
