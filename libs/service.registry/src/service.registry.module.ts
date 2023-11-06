34d51761c8ce4f0a054d1ece2a5f01b438ce45d1
Krasimir Raykov <raykov@weichain.tech>
2023-11-06 09:17:08 +0200

    feat: fix service registry redis connection

--- libs/service.registry/src/service.registry.module.ts
+++ libs/service.registry/src/service.registry.module.ts
@@ -1,20 +1,10 @@
-import { RedisModule } from '@liaoliaots/nestjs-redis';
-import { Config } from '@lib/config';
 import { LoggerModule } from '@lib/logger';
+import { RedisModule } from '@lib/redis';
 import { Module } from '@nestjs/common';
 import { ServiceRegistry } from './service.registry.service';
 
 @Module({
-  imports: [
-    LoggerModule,
-    RedisModule.forRoot({
-      config: {
-        host: Config.REDIS_HOST,
-        port: Config.REDIS_PORT,
-        password: Config.REDIS_PASSWORD,
-      },
-    }),
-  ],
+  imports: [LoggerModule, RedisModule],
   providers: [ServiceRegistry],
   exports: [ServiceRegistry],
 })
